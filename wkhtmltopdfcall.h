b96573557d9f5a0a0fffba230e25f4f780d80537
Ron Hu <Ron@Rons-Mac.local>
2014-08-05 13:38:56 +0800

    init

---
+++ wkhtmltopdfcall.h
@@ -0,0 +1,14 @@
+//
+//  wkhtmltopdfcall.h
+//  wkhtmltopdfcall
+//
+//  Created by Ron Hu on 8/5/14.
+//  Copyright (c) 2014 Techpubs, Inc. All rights reserved.
+//
+
+#ifndef wkhtmltopdfcall_wkhtmltopdfcall_h
+#define wkhtmltopdfcall_wkhtmltopdfcall_h
+
+CAPI(int) htmltopdfcall(const char* html, const char* pdf);
+
+#endif
